425d21c157bdb8d91ad6278dfef18834fa048b0c
mitubaEX <nakamurajun69@gmail.com>
2023-05-31 22:41:38 +0900

    use LAYOUT

--- keyboards/vaut36/keymaps/default/keymap.c
+++ keyboards/vaut36/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x10(
+    [0] = LAYOUT(
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
