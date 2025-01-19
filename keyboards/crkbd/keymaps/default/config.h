e409286fe5c7eaff3a471c64f58fc3ab561cf333
mitubaEX <nakamurajun69@gmail.com>
2025-01-19 18:30:29 +0900

    fix config

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -47,3 +47,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
 #endif
+
+#define DEBOUNCE 5
+
+// Mod Tap Key と通常キーを連続して拘束に押したときに両方のキーを Tap として扱う
+#define HOLD_ON_OTHER_KEY_PRESS
