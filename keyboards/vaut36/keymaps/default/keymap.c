a81e08ef1f9f89b8c05cf3feb2f2b4a4e1b8460e
mitubaEX <nakamurajun69@gmail.com>
2026-03-28 18:25:06 +0900

    Update vaut36 thumb cluster layout

--- keyboards/vaut36/keymaps/default/keymap.c
+++ keyboards/vaut36/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         LGUI_T(KC_A), LCTL_T(KC_S), LSFT_T(KC_D), KC_F,         KC_G,     KC_H, KC_J,         KC_K,         KC_L,         KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                 LT(_NAV, KC_TAB), KC_SPC, OSM(MOD_LSFT), KC_ENT, LT(_SYM, KC_BSPC), LT(_NUM, KC_DEL)
+                 LT(_NAV, KC_ESC), LT(_NUM, KC_TAB), KC_SPC, KC_ENT, LT(_SYM, KC_BSPC), KC_LGUI
     ),
 
     [_NUM] = LAYOUT(
