1e4f2b758551e523ad92b232ac90e1ce67f8617d
mitubaEX <nakamurajun69@gmail.com>
2026-03-29 18:20:48 +0900

    fix keymap

--- keyboards/vaut36/keymaps/default/keymap.c
+++ keyboards/vaut36/keymaps/default/keymap.c
@@ -13,9 +13,9 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        LCTL_T(KC_A), LSFT_T(KC_S), KC_D,         KC_F,         KC_G,     KC_H, KC_J,         KC_K,         RSFT_T(KC_L), RCTL_T(KC_SCLN),
+        LCTL_T(KC_A), KC_S, KC_D,         KC_F,         KC_G,    KC_H, KC_J,         KC_K,         KC_L, RCTL_T(KC_SCLN),
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                 LT(_NAV, KC_ESC), LT(_SYM, KC_TAB), KC_SPC, KC_ENT, LT(_NUM, KC_BSPC), KC_LGUI
+                 LT(_NAV, KC_ESC), LT(_SYM, KC_TAB), KC_SPC, RSFT_T(KC_ENT), LT(_NUM, KC_BSPC), KC_LGUI
     ),
 
     [_NUM] = LAYOUT(
